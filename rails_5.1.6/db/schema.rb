b14fc3a9b27bcf6f2181cbdf56b14234ac3d0acf
ta1kt0me <p.wadachi@gmail.com>
2018-04-11 19:26:35 +0900

    bin/rails db:migrate

--- rails_5.1.6/db/schema.rb
+++ rails_5.1.6/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411102138) do
+ActiveRecord::Schema.define(version: 20180411102608) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "kinds"
   end
 
 end
