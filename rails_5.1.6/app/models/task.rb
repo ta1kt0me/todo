4b18fadea20067d57be3c9424dfaf9a9d1c1feac
ta1kt0me <p.wadachi@gmail.com>
2018-04-11 19:27:54 +0900

    Enalbe active_flag

--- rails_5.1.6/app/models/task.rb
+++ rails_5.1.6/app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+  # See https://github.com/kenn/active_flag#usage
+  flag :kinds, %I[home work buy event]
 end
--- rails_5.1.6/db/seeds.rb
+++ rails_5.1.6/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(name: "炊き込みご飯つくる").tap { |task| task.kinds.set(:home) }.save!
+Task.create(name: "レビューする").tap { |task| task.kinds.set(:work) }.save!
+Task.create(name: "洗剤").tap { |task| task.kinds.set(:work) }.save!
+Task.create(name: "凧揚げ").tap { |task| task.kinds.set(:work) }.save!
