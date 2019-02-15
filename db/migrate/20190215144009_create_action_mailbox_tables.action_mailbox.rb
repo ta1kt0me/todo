7e271014274ba7b57be5d020d26971f8a975e14c
ta1kt0me <p.wadachi@gmail.com>
2019-02-15 23:40:22 +0900

    bin/rails action_mailbox:install

---
+++ db/migrate/20190215144009_create_action_mailbox_tables.action_mailbox.rb
@@ -0,0 +1,18 @@
+# This migration comes from action_mailbox (originally 20180917164000)
+class CreateActionMailboxTables < ActiveRecord::Migration[6.0]
+  def change
+    create_table :action_mailbox_inbound_emails do |t|
+      t.integer :status, default: 0, null: false
+      t.string  :message_id, null: false
+      t.string  :message_checksum, null: false
+
+      if supports_datetime_with_precision?
+        t.timestamps precision: 6
+      else
+        t.timestamps
+      end
+
+      t.index [ :message_id, :message_checksum ], name: "index_action_mailbox_inbound_emails_uniqueness", unique: true
+    end
+  end
+end
